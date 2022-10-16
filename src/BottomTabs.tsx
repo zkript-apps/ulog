0f3f4df1788f80f3700467b5c9be317a31a28504
John Patrick Madrigal <jp.madrigal07@gmail.com>
2022-10-17 07:17:38 +0800

    feat: added about screen for developers info and contacts

--- src/BottomTabs.tsx
+++ src/BottomTabs.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import ClassesScreen from './screens/Classes';
 import StudentsScreen from './screens/Students';
+import AboutScreen from './screens/About';
 import VectorIcon from './components/VectorIcon';
 import { Text } from 'react-native';
 
@@ -52,6 +53,30 @@ function BottomTabs() {
                   }`}
                 >
                   Students
+                </Text>
+              </>
+            );
+          },
+        }}
+      />
+      <Screen
+        name="About"
+        component={AboutScreen}
+        options={{
+          tabBarIcon: ({ focused }) => {
+            return (
+              <>
+                <VectorIcon
+                  name="information-circle"
+                  color={focused ? '#3ABFF8' : '#c9c9c9'}
+                  size={22}
+                />
+                <Text
+                  className={`text-[11px] ${
+                    focused ? 'text-[#3ABFF8]' : 'text-[#c9c9c9]'
+                  }`}
+                >
+                  About
                 </Text>
               </>
             );
